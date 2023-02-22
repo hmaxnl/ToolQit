7a6e76a7c2f9ec5fd2913093cdaaac7d5f451225
Max <51083570+DRdrProfessor@users.noreply.github.com>
2023-02-22 21:40:03 +0100

    Implementing data collection.

--- ToolQit/Collections/DataCollection.cs
+++ ToolQit/Collections/DataCollection.cs
@@ -1,11 +1,17 @@
+using System;
 using System.Collections.Generic;
 
 namespace ToolQit.Collections
 {
     public class DataCollection
     {
+        public DataCollection(char dividerChar = '.')
+        {
+            _dividerChar = dividerChar;
+        }
         private readonly Dictionary<string, DataCollection> _data = new Dictionary<string, DataCollection>();
         private readonly Dictionary<string, string> _contents = new Dictionary<string, string>();
+        private readonly char _dividerChar;
 
         public void SetString(string key, string value) => _contents[key] = value;
 
@@ -15,21 +21,41 @@ namespace ToolQit.Collections
         {
             get
             {
-                if (!_data.ContainsKey(key))
-                    _data.Add(key, new DataCollection());
+                if (key.Contains(_dividerChar))
+                {
+                    AddFromQueue(new KeyQueue(key, _dividerChar));
+                    //TODO: Get the data!
+                }
+
+                CheckAddKey(key);
                 return _data[key];
             }
         }
+
+        private void AddFromQueue(KeyQueue queue)
+        {
+            if (queue.IsEmpty) return;
+            string queueKey = queue.Next();
+            CheckAddKey(queueKey);
+            _data[queueKey].AddFromQueue(queue);
+        }
+
+        private void CheckAddKey(string key)
+        {
+            if (!_data.ContainsKey(key))
+                _data.Add(key, new DataCollection(_dividerChar));
+        }
     }
 
-    class CollectionFinder
+    class KeyQueue
     {
-        public CollectionFinder(string path, char divider = '.')
+        public KeyQueue(string key, char divider = '.')
         {
-            _path = new Queue<string>(path.Split(divider));
+            _key = new Queue<string>(key.Split(divider, StringSplitOptions.RemoveEmptyEntries));
         }
-        public string Next() => _path.Dequeue();
+        public string Next() => _key.Dequeue();
+        public bool IsEmpty => _key.Count <= 0;
 
-        private readonly Queue<string> _path;
+        private readonly Queue<string> _key;
     }
 }
