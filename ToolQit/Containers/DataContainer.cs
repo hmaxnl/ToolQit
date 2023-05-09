af3abae0445b30c3ef58579a18e6e179eaea5986
Max <51083570+DRdrProfessor@users.noreply.github.com>
2023-05-09 20:36:21 +0200

    Updated set functions (Internal repo)

--- ToolQit/Containers/DataContainer.cs
+++ ToolQit/Containers/DataContainer.cs
@@ -14,10 +14,33 @@ namespace ToolQit.Containers
         private readonly Dictionary<string, object> _data = new Dictionary<string, object>();
         public ReadOnlyDictionary<string, object> Data => new ReadOnlyDictionary<string, object>(_data);
 
-        public void Set(string key, string sValue) => _data[key] = sValue;
-        public void Set(string key, long lValue) => _data[key] = lValue;
-        public void Set(string key, double dValue) => _data[key] = dValue;
-        public void Set(string key, bool bValue) => _data[key] = bValue;
+        public void Set(string key, string sValue, bool overridable = true)
+        {
+            if (ContainsKey(key) && !overridable)
+                return;
+            _data[key] = sValue;
+        }
+
+        public void Set(string key, long lValue, bool overridable = true)
+        {
+            if (ContainsKey(key) && !overridable)
+                return;
+            _data[key] = lValue;
+        }
+
+        public void Set(string key, double dValue, bool overridable = true)
+        {
+            if (ContainsKey(key) && !overridable)
+                return;
+            _data[key] = dValue;
+        }
+
+        public void Set(string key, bool bValue, bool overridable = true)
+        {
+            if (ContainsKey(key) && !overridable)
+                return;
+            _data[key] = bValue;
+        }
 
         public bool ContainsKey(string key) => _data.ContainsKey(key);
 
